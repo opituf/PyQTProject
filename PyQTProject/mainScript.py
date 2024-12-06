e3bd151698d25c42a9621b03fd6d551fcbcf5b14
opituf <121101929+opituf@users.noreply.github.com>
2024-12-07 03:00:38 +0900

    1

--- PyQTProject/mainScript.py
+++ PyQTProject/mainScript.py
@@ -1,44 +1,60 @@
-from PyQt6 import uic, QtWidgets
+from PyQt6 import uic, QtWidgets, QtCore, QtGui
 from PyQt6.QtWidgets import QApplication, QWidget, QStackedWidget, QVBoxLayout, QPushButton, QLabel, QMainWindow, \
-    QPushButton, QDialog, QLineEdit
+    QPushButton, QDialog, QLineEdit, QMessageBox, QListView, QListWidget, QScrollArea, QTextEdit
 from PyQt6.uic import loadUi
-from lessons import Ui_lesson1, Ui_lesson2, Ui_lesson3, Ui_lesson4, Ui_lesson5
+import sqlite3
 
 
-class Lesson1(QMainWindow):
+class RegisterWindow(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('ui/lesson1.ui', self)
+        uic.loadUi('ui/registerWindow.ui', self)
+        self.connection = sqlite3.connect("users.db")
+        self.cursor = self.connection.cursor()
 
+        self.password_form = self.findChild(QLineEdit, 'passwordForm')
+        self.name_form = self.findChild(QLineEdit, 'nameForm')
+        self.reg_btn = self.findChild(QPushButton, 'regBtn')
+        self.enter_btn = self.findChild(QPushButton, 'enterBtn')
 
-class Lesson2(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('ui/lesson2.ui', self)
+        self.reg_btn.clicked.connect(self.registration)
+        self.enter_btn.clicked.connect(self.login)
 
+    def registration(self):
+        if self.name_form.text() and self.password_form.text():
+            try:
+                self.cursor.execute(
+                    f"""INSERT INTO users (name, password, lessons) VALUES ({self.name_form.text()}, {self.password_form.text()}, 0)""")
+                self.connection.commit()
+                QMessageBox.information(self, "Успех", "Пользователь успешно создан")
+                self.password_form.clear()
+            except sqlite3.IntegrityError:
+                QMessageBox.warning(self, "Ошибка", "Имя пользователя уже существует.")
+        else:
+            QMessageBox.warning(self, "Ошибка", "Заполните оба поля для регистрации.")
 
-class Lesson3(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('ui/lesson3.ui', self)
-
-
-class Lesson4(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('ui/lesson4.ui', self)
-
-
-class Lesson5(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('ui/lesson5.ui', self)
+    def login(self):
+        if self.name_form.text() and self.password_form.text():
+            check = self.cursor.execute(
+                f"""SELECT * FROM users WHERE name = {self.name_form.text()} and password = {self.password_form.text()}""").fetchall()
+            if len(check) != 0:
+                self.id = check[0][0]
+                self.main_wind = MainWindow(self.id)
+                self.main_wind.show()
+            else:
+                QMessageBox.warning(self, "Ошибка", "Имя или пароль неверны")
+                self.password_form.clear()
+        else:
+            QMessageBox.warning(self, "Ошибка", "Имя или пароль неверны")
+            self.password_form.clear()
+            self.name_form.clear()
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, user_id):
         super().__init__()
         uic.loadUi('ui/main_window.ui', self)
+        self.user_id = user_id  # Сохраняем только id пользователя
 
         self.stacked_wid = self.findChild(QStackedWidget, 'stackedWidget')
         self.page1_btn = self.findChild(QPushButton, 'page1_btn')
@@ -49,22 +65,16 @@ class MainWindow(QMainWindow):
         self.page2_btn.clicked.connect(self.changePage)
         self.page3_btn.clicked.connect(self.changePage)
 
-        self.btn_lesson1 = self.findChild(QPushButton, 'lesson1_btn')
-        self.btn_lesson2 = self.findChild(QPushButton, 'lesson2_btn')
-        self.btn_lesson3 = self.findChild(QPushButton, 'lesson3_btn')
-        self.btn_lesson4 = self.findChild(QPushButton, 'lesson4_btn')
-        self.btn_lesson5 = self.findChild(QPushButton, 'lesson5_btn')
+        self.lesson_list = self.findChild(QListWidget, 'lessonList')
+        self.lesson_list.itemDoubleClicked.connect(self.openLesson)
+        self.showLessons()
 
-        self.btn_lesson1.clicked.connect(self.open_lesson)
-        self.btn_lesson2.clicked.connect(self.open_lesson)
-        self.btn_lesson3.clicked.connect(self.open_lesson)
-        self.btn_lesson4.clicked.connect(self.open_lesson)
-        self.btn_lesson5.clicked.connect(self.open_lesson)
-
-        self.submit_btn = self.findChild(QPushButton, 'submitPrefab')
-        self.prefab = self.findChild(QLineEdit, 'prefab')
-
-        self.submit_btn.clicked.connect(self.savePrefab)
+    def getUserData(self):
+        connection = sqlite3.connect("users.db")
+        cursor = connection.cursor()
+        user_data = cursor.execute("SELECT * FROM users WHERE id = ?", (self.user_id,)).fetchone()
+        connection.close()
+        return user_data
 
     def changePage(self):
         sender = self.sender()
@@ -75,35 +85,95 @@ class MainWindow(QMainWindow):
         else:
             self.stacked_wid.setCurrentIndex(2)
 
-    def open_lesson(self):
-        sender = self.sender()
-        self.new_window = Lesson1()
-        if sender == self.btn_lesson1:
-            self.new_window.show()
-        elif sender == self.btn_lesson2:
-            self.new_window = Lesson2()
-            self.new_window.show()
-        elif sender == self.btn_lesson3:
-            self.new_window = Lesson3()
-            self.new_window.show()
-        elif sender == self.btn_lesson4:
-            self.new_window = Lesson4()
-            self.new_window.show()
-        elif sender == self.btn_lesson5:
-            self.new_window = Lesson5()
-            self.new_window.show()
+    def showLessons(self):
+        connection = sqlite3.connect("lessons.db")
+        cursor = connection.cursor()
+        user_data = self.getUserData()  # Получаем данные пользователя
+        lessons = user_data[3]  # Получаем строку с уроками
+        if lessons:
+            lesson_ids = lessons.split(', ')
+        else:
+            lesson_ids = [0]
+        for i in lesson_ids:
+            try:
+                lesson = cursor.execute(f"""SELECT name FROM lessons WHERE id = {i}""").fetchone()
+                if lesson:
+                    self.lesson_list.addItem(lesson[0])
+            except Exception as e:
+                print(f"Ошибка при получении урока: {e}")
 
-    def savePrefab(self):
-        prefab_text = self.prefab.text()
-        if prefab_text:
-            with open("prefabs.txt", "a", encoding="utf-8") as file:
-                file.write("\n" + prefab_text)
-        self.prefab.clear()
-        self.stacked_wid.setCurrentIndex(1)
+    def openLesson(self, item):
+        label = item.text()
+
+        # Получаем урок
+        connectionLes = sqlite3.connect("lessons.db")
+        connectionUser = sqlite3.connect("users.db")
+        cursorLes = connectionLes.cursor()
+        cursorUser = connectionUser.cursor()
+
+        lesson = cursorLes.execute("SELECT text, id FROM lessons WHERE name = ?", (label,)).fetchall()
+        lesson_text = lesson[0][0]
+        lesson_id = lesson[0][1]
+
+        # Получаем данные о текущих уроках пользователя
+        user_data = self.getUserData()
+        current_lessons = user_data[3]
+        if current_lessons:
+            lesson_ids = list(map(int, current_lessons.split(', ')))
+        else:
+            lesson_ids = [0]
+
+        # Добавляем новый урок, если он не добавлен
+        if lesson_id + 1 not in lesson_ids:
+            new_lesson_id = lesson_id + 1
+            lesson_ids.append(new_lesson_id)
+            updated_lessons = ', '.join(map(str, lesson_ids))
+
+            # Обновляем данные пользователя в базе данных
+            cursorUser.execute(
+                """UPDATE users SET lessons = ? WHERE id = ?""", (updated_lessons, self.user_id)
+            )
+            connectionUser.commit()
+
+        # Открываем окно с текстом урока
+        self.text_book = TextBook(label, lesson_text)
+        self.text_book.show()
+
+        self.lesson_list.clear()
+        self.showLessons()
+
+
+class TextBook(QWidget):
+    def __init__(self, label, text):
+        super().__init__()
+
+        # Устанавливаем заголовок окна
+        self.setWindowTitle(f"Тема: {label}")
+
+        # Создаем вертикальный layout для размещения виджетов
+        layout = QVBoxLayout()
+
+        # Создаем и настраиваем заголовок
+        title_label = QLabel(f"<b>{label}</b>")
+        title_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        # Создаем и настраиваем текстовый редактор
+        self.text_edit = QTextEdit(self)
+        text = text.replace(r"\n", "\n")  # Заменяем строковые символы \n на реальные переносы строк
+        self.text_edit.setPlainText(text)
+        self.text_edit.setReadOnly(True)  # Делаем редактор только для чтения
+        self.text_edit.setWordWrapMode(QtGui.QTextOption.WrapMode.WordWrap)  # Включаем перенос слов
+
+        # Добавляем виджеты в layout
+        layout.addWidget(title_label)
+        layout.addWidget(self.text_edit)
+
+        # Устанавливаем layout для окна
+        self.setLayout(layout)
 
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = MainWindow()
+    window = RegisterWindow()
     window.show()
     app.exec()
